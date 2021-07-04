fef652d40717b3fa97af61c395d8d9de5ce89c83
AlexanderRodgers <alexedrodgers@gmail.com>
2021-07-03 19:59:18 -0700

    Added sparseVector (leetcode 1570)

---
+++ sparseVector-hs.py
@@ -0,0 +1,21 @@
+from typing import List
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.nonzeros = {}
+        for i, n in enumerate(nums):
+            if n != 0:
+                self.nonzeros[i] = n              
+
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        result = 0
+        # iterate through each non-zero element in this sparse vector
+        # update the dot product if the corresponding index has a non-zero value in the other vector
+        for i, n in self.nonzeros.items():
+            if i in vec.nonzeros:
+                result += n * vec.nonzeros[i]
+        return result
+
+nums = [1,0,0,2,3]
+tc = [0,3,0,4,0]
+sv = SparseVector(nums)
+sv.dotProduct(tc)
